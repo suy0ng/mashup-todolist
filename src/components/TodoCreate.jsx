9a2610bfb6d5b28fa18a6dab364dc28f41661538
suy0ng <yunsuyong739@naver.com>
2023-03-06 22:58:54 +0900

    add todoCreate(last commit)

--- src/components/TodoCreate.jsx
+++ src/components/TodoCreate.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { MdAdd } from "react-icons/md";
 import styled, { css } from "styled-components";
+import { useTodoDispatch, useTodoNextId } from "../TodoContext";
 
 const CircleButton = styled.button`
   background: #38d9a9;
@@ -32,7 +33,7 @@ const CircleButton = styled.button`
   justify-content: center;
 
   transition: 0.125s all ease-in;
-  ${props =>
+  ${(props) =>
     props.open &&
     css`
       background: #ff6b6b;
@@ -74,18 +75,40 @@ const Input = styled.input`
   box-sizing: border-box;
 `;
 
-function TodoCreate(props) {
-  const [open, setopen] = useState(false);
-  const onToggle = () => setopen(!open);
+function TodoCreate() {
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState('');
+
+  const dispatch = useTodoDispatch();
+  const nextId = useTodoNextId();
+
+  const onToggle = () => setOpen(!open);
+  const onChange = e => setValue(e.target.value);
+  const onSubmit = e => {
+    e.preventDefault();
+    dispatch({
+      type: "CREATE",
+      todo: {
+        id: nextId.current,
+        text: value,
+        done: false
+      }
+    });
+    setValue('');
+    setOpen(false);
+    nextId.current += 1;
+  };
 
   return (
     <>
       {open && (
         <InsertFormPositioner>
-          <InsertForm>
-            <Input
-              autofocus
-              placeholder="할 일을 입력후, Enter 를 누르세요!"
+          <InsertForm onSubmit={onSubmit}>
+             <Input
+              autoFocus
+              placeholder="할 일을 입력 후, Enter 를 누르세요"
+              onChange={onChange}
+              value={value}
             />
           </InsertForm>
         </InsertFormPositioner>
@@ -97,4 +120,4 @@ function TodoCreate(props) {
   );
 }
 
-export default TodoCreate;
+export default React.memo(TodoCreate);
