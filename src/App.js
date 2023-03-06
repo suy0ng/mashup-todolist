90a67c4fdf1a9966851b8be48cf3271a02a4d26a
suy0ng <yunsuyong739@naver.com>
2023-03-06 13:21:53 +0900

    add TodoHead

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 import { createGlobalStyle } from "styled-components";
 import TodoTemplate from "./components/TodoTemplate";
+import TodoHead from "./components/TodoHead";
 
 function App() {
   const GlobalStyle = createGlobalStyle`
@@ -11,7 +12,9 @@ function App() {
   return (
     <>
       <GlobalStyle />
-      <TodoTemplate>안녕하세요</TodoTemplate>
+      <TodoTemplate>
+        <TodoHead />
+      </TodoTemplate>
     </>
   );
 }
