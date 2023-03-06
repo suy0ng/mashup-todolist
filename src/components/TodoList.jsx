719b00c03bd9ea5f25f9e3e792bfd669c002a094
suy0ng <yunsuyong739@naver.com>
2023-03-06 21:14:26 +0900

    add todoList

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,23 +1,30 @@
-import React from 'react';
-import styled from 'styled-components';
-import TodoItem from './TodoItem';
+import React, { useContext } from "react";
+import styled from "styled-components";
+import { useTodoState } from "../TodoContext";
+import TodoItem from "./TodoItem";
 
 const TodoListBlock = styled.div`
-    flex:1;
-    padding : 20px 32px;
-    padding-bottom : 48px;
-    overflow-y : auto;
+  flex: 1;
+  padding: 20px 32px;
+  padding-bottom: 48px;
+  overflow-y: auto;
 `;
 
 function TodoList(props) {
-    return (
-        <TodoListBlock>
-            <TodoItem text="프로젝트 생성하기" done={true} />
-            <TodoItem text="컴포넌트 스타일링 하기" done={true} />
-            <TodoItem text="Context 만들기" done={false} />
-            <TodoItem text="기능 구현하기" done={false} />
-        </TodoListBlock>
-    );
+  const todos = useTodoState();
+
+  return (
+    <TodoListBlock>
+      {todos.map((todo) => (
+        <TodoItem
+          key={todo.key}
+          id={todo.id}
+          done={todo.done}
+          text={todo.text}
+        />
+      ))}
+    </TodoListBlock>
+  );
 }
 
 export default TodoList;
